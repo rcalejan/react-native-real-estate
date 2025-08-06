3e635af25a9ff493cae5830f22e623383bfcde0e
Conrad Alejandre <conradalejandre@CradMac.local>
2025-08-05 21:53:31 -0700

    Initial commit of project folder for real estate app.

---
+++ app/(root)/properties/[id].tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Property = () => {
+    const { id } = useLocalSearchParams();
+    return (
+        <View>
+            <Text>Property</Text>
+        </View>
+    )
+}
+
+export default Property;
